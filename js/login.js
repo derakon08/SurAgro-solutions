a52826a0aa09cbb4d57daf6b249e45958a8602fd
dera <derakon08@gmail.com>
2026-04-03 15:42:54 -0500

    Initial commit: Full CSS for log in main_page
    It also includes btn_alt css, and a wip list_flat.
    In general this is in hopes of stopping the "late by x days" from going
    any higer.

---
+++ js/login.js
@@ -0,0 +1,20 @@
+export class Login
+{
+    constructor() {};
+    
+    static LoadPage(sidebar, main_content, top_nav)
+        {
+            sidebar.innerHTML = '<div class="card"><h1 class="title">SurAgro Solutions</h1><h2 class="title">Sistema de visualizacion de inventario</h2></div><img src="icon.png" alt="Logo de la empresa suragro"/>';
+            main_content.innerHTML = '<h1 class="title">Por favor, ingrese sus credenciales</h1><div class="card"><div><h2 class="title">Usuario</h2><input type="number"/></div><div><h2 class="title">Contraseña</h2><input type="text"/></div><div class="btn_vlist"><button type="submit" data-action="login" class="btn_standard auth_action">Confirmar</button></div></div><div class="spacer"></div>';
+            top_nav.innerHTML = "";
+
+            return "Inicio De Sesion"
+        } 
+
+    static AuthAction(usr_key)
+    {
+        usr_key = "valid, lol"
+
+        return "dashboard"
+    }
+}
